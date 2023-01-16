ad2c105c759455c149a09f41c538e3ea21439a50
btmarouane <btgmarouane@gmail.com>
2023-01-16 23:53:28 +0100

    debug

--- bb.sh
+++ bb.sh
@@ -496,7 +496,7 @@ uninstall (){
     fi
 
     if [[ "X${domain}" != "X" ]]; then
-	    extra_arg="-e \"domain=$domain all=$([[ "$all" == "all" ]] && echo "true" || echo "false") apps=$tags_arg\""
+	    extra_arg="-e domain=$domain -e all=$([[ "$all" == "all" ]] && echo "true" || echo "false") -e apps=$tags_arg"
     fi
 
     local arguments_bb="--tags uninstall"
